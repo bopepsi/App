37c9fb95d8d55c5f86094222f4cad2e8624740c4
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-07-01 23:24:18 -0400

    edit-comment_model

--- models/comment.js
+++ models/comment.js
@@ -4,6 +4,7 @@ const { Schema } = require('mongoose');
 // Blue print for collection document data
 const commentSchema = new Schema({
     text: { type: String, required: true },
+    date: { type: Date, default: Date.now },
     creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
     replies: [{ type: mongoose.Types.ObjectId, required: true, ref: "Comment" }]
 });
