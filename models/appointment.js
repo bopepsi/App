cb2389d2fa5e0cfb8922bbbf8385d2c8fb3ff0a3
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-07-07 16:39:38 -0400

    fix-appointment-model

--- models/appointment.js
+++ models/appointment.js
@@ -6,8 +6,12 @@ const appointmentSchema = new Schema({
     reciever: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
     creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
     title: { type: String, required: true },
+    desscription: { type: String, required: true },
     address: { type: String, required: true },
-    date: { type: Date, required: true }
+    appointmentData: { type: Date, required: true },
+    recieverAccepted: { type: Boolean, required: true },
+    recieverRejected: { type: Boolean, required: true },
+    date: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Appointment', appointmentSchema);
