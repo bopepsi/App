c26e335c558321996b2cb6a1465cb0659572c714
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-07-07 22:46:45 -0400

    finish-addPostsToCollection-controller

--- controllers/collections-controller.js
+++ controllers/collections-controller.js
@@ -1,6 +1,7 @@
 const { default: mongoose } = require('mongoose');
 const Collection = require('../models/collection');
 const User = require('../models/user');
+const Post = require('../models/post');
 const HttpError = require('../models/http-error');
 
 const getCollectionsByUid = async (req, res, next) => {
@@ -48,7 +49,42 @@ const createCollection = async (req, res, next) => {
 
 const addPostToCollection = async (req, res, next) => {
     const { cid, pid } = req.params;
-    res.json({ cid, pid });
+    console.log(cid, pid);
+    //* get collection
+    let collection;
+    try {
+        collection = await Collection.findById(cid).populate('posts');
+    } catch (error) {
+        return next(new HttpError('Oops something is wrong.', 500));
+    }
+    if (!collection) {
+        return next(new HttpError('Could not find collection.', 404));
+    }
+    //* get post
+    let post;
+    try {
+        post = await Post.findById(pid);
+    } catch (error) {
+        return next(new HttpError('Oops something is wrong.', 500));
+    }
+    if (!post) {
+        return next(new HttpError('Could not find post.', 404));
+    }
+    //* add post ObjectId to collection's posts arr and increase post collections by one
+    post.collections = Number(post.collections) + 1;
+    console.log(post.collections);
+    try {
+        const sess = await mongoose.startSession();
+        sess.startTransaction();
+        await post.save({ session: sess });
+        collection.posts.push(post);
+        await collection.save({ session: sess })
+        await sess.commitTransaction();
+    } catch (error) {
+        return next(new HttpError('Could not add post to collection.', 500));
+    }
+
+    res.json({ message: 'Post added to collection.' });
 }
 
 const removePostFromCollection = async (req, res, next) => {
