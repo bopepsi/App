3d499cb1b2c740365f05c78318dfdf0dc091e555
Bowen Jiang <bopepsi@jiangguangweideMacBook-Pro.local>
2022-07-09 15:57:23 -0400

    fix-cors

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ const app = express();
 //? Parsing incoming body and extract json data
 app.use(bodyParser.json());
 
+//* CORES
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With , Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+})
+
 app.use('/api/posts', postsRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/collections', collectionsRoutes);
